17897ba3ee57e09de2e2fb979a8e097f5a6c2df5
rejunity <rej@unity3d.com>
2016-02-06 10:44:46 +0100

    Automated build script

---
+++ Assets/Editor/MakeBuilds.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Collections;
+
+public class MakeBuilds : Editor {
+	struct NamedTarget
+	{
+		public BuildTarget build;
+		public string name;
+	};
+	public static string Name = "Coven";
+
+	[MenuItem("Coven/Build All Platforms")]
+	static public void BuildAllPlatforms ()
+	{
+		string[] levels = {}; // current level
+	
+		NamedTarget[] targets = new NamedTarget[] {
+			new NamedTarget { build=BuildTarget.WebPlayer,			name="WebPlayer" } ,
+			new NamedTarget { build=BuildTarget.WebGL,				name="HTML5" } ,
+			new NamedTarget { build=BuildTarget.StandaloneOSXIntel,	name="OSX" } ,
+			new NamedTarget { build=BuildTarget.StandaloneWindows,	name="PC" } };
+
+		foreach (var t in targets)
+		{
+			//if (!EditorUserBuildSettings.SwitchActiveBuildTarget (t))
+			//	continue;
+
+			BuildPipeline.BuildPlayer (levels, Path.Combine("Builds", Name + "_" + t.name), t.build, BuildOptions.None);
+		}
+	}
+}
